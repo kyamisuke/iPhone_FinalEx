767dd4077a1dd975be2c86e4b69d9254a48405fe
kyamisuke <koukou0301@gmail.com>
2019-09-30 15:55:31 +0900

    Initial Commit

---
+++ c4_practise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  c4_practise
+//
+//  Created by 村上航輔 on 2019/09/30.
+//  Copyright © 2019 kyamisuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ c4_practiseTests/c4_practiseTests.swift
@@ -0,0 +1,34 @@
+//
+//  c4_practiseTests.swift
+//  c4_practiseTests
+//
+//  Created by 村上航輔 on 2019/09/30.
+//  Copyright © 2019 kyamisuke. All rights reserved.
+//
+
+import XCTest
+@testable import c4_practise
+
+class c4_practiseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
